bc80ffb9a801834e24f4916a6d2167be46113399
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-10-12 00:32:57 +0300

    Fetch & display documents

--- routes/blog.js
+++ routes/blog.js
@@ -7,20 +7,26 @@ const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get('/posts', async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection('posts')
+    .find({})
+    .project({ title: 1, summary: 1, 'author.name': 1 })
+    .toArray();
+  res.render('posts-list', { posts: posts });
 });
 
-router.get('/new-post', async function(req, res) {
+router.get('/new-post', async function (req, res) {
   const authors = await db.getDb().collection('authors').find().toArray();
   res.render('create-post', { authors: authors });
 });
 
-router.post('/posts', async function(req, res) {
+router.post('/posts', async function (req, res) {
   const authorId = new ObjectId(req.body.author);
   const author = await db.getDb().collection('authors').findOne({ _id: authorId });
 
